1025f0d968c9d3e334ed537113cd34b31915dc3d
roehd <kieun0125@gmail.com>
2023-05-02 14:21:22 +0900

    AppConfig Refactoring - 역할과 구현 클래스 분리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,17 +1,31 @@
 package hello.core;
 
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+
+        return new MemberServiceImpl(
+                getMemberRepository());
     }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(
+                getMemberRepository(),
+                getDiscountPolicy());
+    }
+
+    private static DiscountPolicy getDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+    private static MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
     }
 }
